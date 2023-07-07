e2aea27c27c01129c9b7d7eac6379974566bd1de
unknown <faiz.jamaludin02@gmail.com>
2023-07-08 01:08:17 +0800

    modify booking Models. add address and change booking date type to date instead of string

--- src/Models/bookingModel.js
+++ src/Models/bookingModel.js
@@ -1,20 +1,21 @@
 const mongoose = require("mongoose");
 
 const bookingSchema = new mongoose.Schema({
-    name: String,
-    email: String,
-    phone: String,
-    bookingDate: String,
-    key: String,
-    status: {
-        type: String,
-        default: 'Pending'
-    },
-    reason: {
-        type: String,
-        default: ''
-    },
-    date: { type: Date, default: Date.now },
+  name: String,
+  email: String,
+  phone: String,
+  bookingDate: Date,
+  address: String,
+  key: String,
+  status: {
+    type: String,
+    default: "Pending",
+  },
+  reason: {
+    type: String,
+    default: "",
+  },
+  date: { type: Date, default: Date.now },
 });
 
 const Booking = mongoose.model("Booking", bookingSchema);
--- src/Services/booking.service.js
+++ src/Services/booking.service.js
@@ -1,77 +1,73 @@
-const Booking = require('../Models/bookingModel')
-const nodemailer = require('nodemailer');
+const Booking = require("../Models/bookingModel");
+const nodemailer = require("nodemailer");
 
 // create transporter auth to send an email
 
 const transporter = nodemailer.createTransport({
-    service: 'Gmail',
-    auth: {
-        user: 'faiz.jamaludin02@gmail.com',
-        pass: process.env.GMAIL_PASS,
-    },
+  service: "Gmail",
+  auth: {
+    user: "faiz.jamaludin02@gmail.com",
+    pass: process.env.GMAIL_PASS,
+  },
 });
 
 const createBooking = (data, res) => {
-    const booking = new Booking({
-        name: data.name,
-        email: data.email,
-        phone: data.phone,
-        bookingDate: data.booking,
-        key: data.key
+  const booking = new Booking({
+    name: data.name,
+    email: data.email,
+    phone: data.phone,
+    bookingDate: data.booking,
+    address: data.address,
+    key: data.key,
+  });
+
+  if (booking.save()) {
+    const msg = "your key is " + data.key;
+
+    const mailOptions = {
+      from: "faiz.jamaludin02@gmail.com",
+      to: data.email,
+      subject: "Thank you for submitting",
+      text: msg,
+    };
+
+    // Send the email
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.error(error);
+        res.sendStatus(500);
+      } else {
+        console.log("Email sent: " + info.response);
+        res.sendStatus(200);
+      }
     });
-
-
-    if (booking.save()) {
-        const msg = 'your key is ' + data.key;
-
-
-        const mailOptions = {
-            from: 'faiz.jamaludin02@gmail.com',
-            to: data.email,
-            subject: 'Thank you for submitting',
-            text: msg,
-        };
-
-        // Send the email
-        transporter.sendMail(mailOptions, (error, info) => {
-            if (error) {
-                console.error(error);
-                res.sendStatus(500);
-            } else {
-                console.log('Email sent: ' + info.response);
-                res.sendStatus(200);
-            }
-        });
-    }
-
-}
+  }
+};
 
 const getBooking = async (data) => {
-    const booking = await Booking.findOne({ key: data })
+  const booking = await Booking.findOne({ key: data });
 
-    bookingData = {
-        name: booking.name,
-        phone: booking.phone,
-        bookingDate: booking.bookingDate,
-        status: booking.status
-    }
+  bookingData = {
+    name: booking.name,
+    phone: booking.phone,
+    bookingDate: booking.bookingDate,
+    status: booking.status,
+  };
 
-    // console.log(bookingData)
+  // console.log(bookingData)
 
-    return bookingData;
-
-
-}
+  return bookingData;
+};
 
 const getAllBooking = async () => {
-    const booking = await Booking.find();
+  const booking = await Booking.find();
 
-    // console.log(booking);
-    return booking;
-}
+  // console.log(booking);
+  return booking;
+};
 
 module.exports = {
-    createBooking,
-    getBooking,
-    getAllBooking
+  createBooking,
+  getBooking,
+  getAllBooking,
 };
